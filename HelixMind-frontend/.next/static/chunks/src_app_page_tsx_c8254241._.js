6684feaddda36e84f9f12aa79f6111448a6391d0
Raghav khanna <khannaraghav90@gmail.com>
2026-05-10 20:04:18 +0530

    Add frontend scaffold and environment files

---
+++ HelixMind-frontend/.next/static/chunks/src_app_page_tsx_c8254241._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e2730689._.js",
+  "static/chunks/node_modules_f642e35f._.js"
+],
+    source: "dynamic"
+});
